5de7fba979e82c39769a287c22a05903d2f35af8
zachli359 <81203486+zachli359@users.noreply.github.com>
2022-02-28 08:36:10 -0500

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,11 @@
 def main():
-    #your code goes here, make sure it's indented
-    
+  #your code goes here, make sure it's indented
+  siblings=int(input("enter your number of siblings\n"))
+  colour=input("enter your favourite colour\n")
+
+  print("if you had another sibling, you would      have",siblings+1)
+  print("if you had triple the siblings, you would have",siblings*3)
+  print("If you and your siblings were split into 2 teams, you would have",siblings//2,"on each team and",siblings%2,"left over")
 
     
 #Leave this code here    
